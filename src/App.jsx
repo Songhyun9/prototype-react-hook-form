70ba31e907f3d5bb7e3575f4f2e8c94ec2c80478
Songhyun9 <34291021+Songhyun9@users.noreply.github.com>
2022-12-27 14:10:45 +0900

    4 files modified, 1 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { useState } from 'react';
+import Mui from './Mui';
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Mui />
+    </div>
+  );
 }
 
 export default App;
